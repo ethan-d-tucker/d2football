168b16e71707cf5f08ae51ef0b8f0ca29ba095b8
ethan-d-tucker <141704000+ethan-d-tucker@users.noreply.github.com>
2025-09-06 21:55:47 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple SW
-const CACHE='miaa-v4'; const SHELL=['./','./index.html','./standings.html','./rankings.html','./assets/css/styles.css','./assets/js/scores.js','./assets/js/standings.js','./assets/js/rankings.js'];
+const CACHE='miaa-v9'; const SHELL=['./','./index.html','./standings.html','./rankings.html','./assets/css/styles.css','./assets/js/scores.js','./assets/js/standings.js','./assets/js/rankings.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))); self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim();});
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url); if(u.pathname.startsWith('/api/')){e.respondWith(fetch(e.request)); return;} e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
